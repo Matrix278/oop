2dc35129d76ef1e11d7006739b65a092ff135212
Martin Sidorov <martin.sidorov@wallester.com>
2026-08-07 12:53:42 +0300

    Add animal polymorphism lesson

--- Main.java
+++ Main.java
@@ -1,17 +1,30 @@
 
 public class Main {
 
+    static void makeItSpeak(Animal animal) {
+        animal.speak();
+    }
+
     public static void main(String[] args) {
         Cat milo = new Cat("Milo", 2);
         Cat luna = new Cat("Luna", 5);
+        Dog rex = new Dog("Rex");
 
         milo.haveBirthday();
-        milo.meow();
-        luna.meow();
+        milo.speak();
+        luna.speak();
+
+        makeItSpeak(milo);
+        makeItSpeak(rex);
     }
 }
 
-class Cat {
+interface Animal {
+
+    void speak();
+}
+
+class Cat implements Animal {
 
     private String name;
     private int age;
@@ -21,7 +34,7 @@ class Cat {
         this.age = age;
     }
 
-    public void meow() {
+    public void speak() {
         System.out.println(name + " is " + age + " years old and says meow!");
     }
 
@@ -29,5 +42,19 @@ class Cat {
         age = age + 1;
 
         System.out.println(name + " is now " + age + " years old!");
+    }
+
+}
+
+class Dog implements Animal {
+
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public void speak() {
+        System.out.println(name + " says woof!");
     }
 }
